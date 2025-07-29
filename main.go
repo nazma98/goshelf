bc4d2a12b954d413bf5ce5cd6cd43dc450c8f9aa
nazma98 <nazmasarker1311@gmail.com>
2025-07-29 23:57:41 +0600

    Add GetBook method

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -9,11 +10,23 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "📚 Welcome to GoShelf API!")
 }
 
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "PLease give a GET method", 400)
+		return
+	}
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(bookList)
+}
+
 type Book struct {
 	ID     int    `json:"id"`
 	Title  string `json:"title"`
 	Author string `json:"author"`
 }
+
+var bookList []Book
 
 func main() {
 	mux := http.NewServeMux()
@@ -24,4 +37,21 @@ func main() {
 	if err != nil {
 		fmt.Println("❌ Server failed to run", err)
 	}
+}
+
+func init() {
+	book1 := Book{
+		ID:     1,
+		Title:  "The Go Programming Language",
+		Author: "Alan A. A. Donovan",
+	}
+	book2 := Book{
+		ID:     1,
+		Title:  "Introducing Go",
+		Author: "Caleb Doxsey",
+	}
+
+	bookList = append(bookList, book1)
+	bookList = append(bookList, book2)
+
 }
